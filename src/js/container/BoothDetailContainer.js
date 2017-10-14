4866cb2f35f91dae31712945ff74fb4022014d78
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-10-14 17:46:13 +0900

    arrange booth detail view

--- src/js/container/BoothDetailContainer.js
+++ src/js/container/BoothDetailContainer.js
@@ -4,30 +4,53 @@ import {goBack} from 'react-router-redux';
 
 import AppBar from 'material-ui/AppBar';
 import {Card, CardActions, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import CircularProgress from 'material-ui/CircularProgress';
 import IconButton from 'material-ui/IconButton';
 import ArrowBackIcon from 'material-ui/svg-icons/navigation/arrow-back';
 import FavoriteIcon from 'material-ui/svg-icons/action/favorite';
+import FavoriteBorderIcon from 'material-ui/svg-icons/action/favorite-border';
 import VisibilityIcon from 'material-ui/svg-icons/action/visibility';
 import VisibilityOffIcon from 'material-ui/svg-icons/action/visibility-off';
 import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
 
 import {loadBooths, postBoothLike} from '../modules/firebase';
 
-const LikeButton = (props) => (
-    <RaisedButton
-        fullWidth
-        primary={true}
-        buttonStyle={{
-            height: 150,
-            margin: 20
-        }}
-        label="Like"/>
-);
+const BoothCard = (props) => {
+    const {booth} = props;
+    return (
+        <Card>
+            <CardMedia overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}>
+                <img src="images/nature-600-337.jpg" alt=""/>
+            </CardMedia>
+            <CardTitle
+                title={booth.name}
+                subtitle={booth.locationName}/>
+            <CardText>
+                {booth.description}
+            </CardText>
+            <CardActions>
+                <FlatButton
+                    label={booth.likes.size}
+                    icon={<FavoriteIcon/>}
+                />
+                <FlatButton
+                    label={booth.visibilities.size}
+                    icon={<VisibilityIcon/>}
+                />
+            </CardActions>
+        </Card>)
+};
 
 class BoothDetailContainer extends React.Component {
     handleClickBack = () => {
         this.props.goBack();
+    };
+
+    handleLickButtonClick = () => {
+        this.props.postLike();
+    };
+
+    handleVisibilityButtonClick = () => {
     };
 
     getStyles = () => {
@@ -54,48 +77,27 @@ class BoothDetailContainer extends React.Component {
     render() {
         const styles = this.getStyles();
         const {booth, postLike} = this.props;
-        console.log(booth && booth.likes.size);
+
+        const appBarIconContainer = (
+            <div>
+                <IconButton onClick={this.handleLickButtonClick}><FavoriteBorderIcon/></IconButton>
+                <IconButton onClick={this.handleVisibilityButtonClick}><VisibilityIcon/></IconButton>
+            </div>
+        );
+
         return (
             <div>
                 <AppBar
                     iconElementLeft={<IconButton><ArrowBackIcon/></IconButton>}
                     onLeftIconButtonTouchTap={this.handleClickBack}
+                    iconElementRight={appBarIconContainer}
                     title="Detail"/>
 
-                <Card style={styles.content}>
-                    <CardMedia overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle"/>}>
-                        <img src="images/nature-600-337.jpg" alt=""/>
-                    </CardMedia>
-                    <CardTitle
-                        title={booth ? booth.name : 'loading'}
-                        subtitle={booth ? booth.locationName : 'loading'}/>
-                    <CardText>
-                        {booth ? booth.description : 'loading'}
-                    </CardText>
-                    <CardActions>
-                        <FlatButton
-                            label={booth ? booth.likes.size : 0}
-                            icon={<FavoriteIcon/>}
-                        />
-                        <FlatButton
-                            label={booth ? booth.visibilities.size : 0}
-                            icon={<VisibilityIcon/>}
-                        />
-                    </CardActions>
-                </Card>
-
-                <RaisedButton
-                    primary
-                    label="Like"
-                    onClick={postLike}
-                    style={{
-                        width: '80%',
-                        margin: 20,
-                    }}
-                    buttonStyle={{
-                        height: 150,
-                    }}/>
-
+                <div style={styles.content}>
+                    {booth ?
+                        (<BoothCard booth={booth}/>) :
+                        (<CircularProgress/>)}
+                </div>
             </div>
         )
     }
