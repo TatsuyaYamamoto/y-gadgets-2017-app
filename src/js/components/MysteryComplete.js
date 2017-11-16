2fc2c6963afaa4a24ed590990cae61ea3d6b5aaa
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-11-16 23:30:58 +0900

    arranga mystery css

--- src/js/components/MysteryComplete.js
+++ src/js/components/MysteryComplete.js
@@ -2,9 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const MysteryComplete = (props) => {
+    const styles = {
+        completeText:{
+            fontSize: 100
+        }
+    };
     return (
         <div>
-            正解！
+            <div style={styles.completeText}>正解！</div>
+            <div>全問正解おめでとう！富士ゼロックス R&Dスクエアの触覚屋でお菓子を貰おう！<br/>＊触覚屋ブースの情報</div>
         </div>
     )
 };
